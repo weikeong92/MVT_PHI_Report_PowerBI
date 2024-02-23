24b87d1ffce3a961194433f9f52754413a834c2a
weikeong92 <124226156+weikeong92@users.noreply.github.com>
2024-02-23 18:03:19 +0800

    update the script

--- CSV_Validator.py
+++ CSV_Validator.py
@@ -16,45 +16,51 @@ def replace_header(csv_file):
             rows = list(csv.reader(file))
             rows[0] = header
 
-            for row in rows[1:]:
-                for col_idx in [13, 16, 17]:
-                    if row[col_idx] == '#DIV/0!':
-                        row[col_idx] = '0'
-
-                odm_idx = header.index('ODM')
-                if row[odm_idx].strip() == "GemTek":
-                    row[odm_idx] = "Gemtek"
-
-                input_idx = header.index('Input')
-                if row[input_idx].strip() == '-' or row[input_idx].strip() == "NO PRF":
-                    row[input_idx] = '0'
+            for row_idx, row in enumerate(rows[1:], start=1):
+                try:
+                    for col_idx in [13, 16, 17]:
+                        if row[col_idx] == '#DIV/0!':
+                            row[col_idx] = '0'
+                except IndexError:
+                    #print(f"Error: Index out of range in row column {col_idx}")
+                    continue
                 
-                pareto_idx = header.index('Pareto')
-                if row[pareto_idx].strip() == '0':
-                    row[pareto_idx] = 'NA'
+                try:
+                    for col in [0, 5, 10, 11, 13, 14, 18, 23]:
+                        odm_idx = header.index('ODM')
+                        if row[odm_idx].strip() == "GemTek":
+                            row[odm_idx] = "Gemtek"
 
-                qty_idx = header.index('Qty')
-                if row[qty_idx].strip() == '-' or row[qty_idx].strip() == "#VALUE!":
-                    row[qty_idx] = '0'
-                elif row[qty_idx].startswith("-") and row[qty_idx][1:].isdigit():
-                    row[qty_idx] = '0'
+                        input_idx = header.index('Input')
+                        if row[input_idx].strip() == '-' or row[input_idx].strip() == "NO PRF":
+                            row[input_idx] = '0'
 
-                yield_idx = header.index('Yield')
-                if row[yield_idx].strip() == "#VALUE!" or row[yield_idx].strip() == "#DIV/0!":
-                    row[yield_idx] = '0%'
-                elif row[yield_idx].startswith("-") and row[yield_idx][1:].isdigit():
-                    row[yield_idx] = '0'
+                        pareto_idx = header.index('Pareto')
+                        if row[pareto_idx].strip() == '0':
+                            row[pareto_idx] = 'NA'
 
-                alpha_idx = header.index('Alpha')
-                if row[alpha_idx].strip() == "#DIV/0!":
-                    row[alpha_idx] = '0%'
+                        qty_idx = header.index('Qty')
+                        if row[qty_idx].strip().startswith('-') or row[qty_idx].strip() == "#VALUE!":
+                            row[qty_idx] = '0'
 
-                ndfdpm_idx = header.index('NDF dpm')
-                if row[ndfdpm_idx].strip() == "#DIV/0!":
-                    row[ndfdpm_idx] = '0'
-                
-                test_time_idx = header.index('Test Time')
-                row[test_time_idx] = row[test_time_idx].replace('S', '')
+                        yield_idx = header.index('Yield')
+                        if row[yield_idx].strip() == "#VALUE!" or row[yield_idx].strip() == "#DIV/0!":
+                            row[yield_idx] = '0.00%'
+                        elif row[yield_idx].strip().startswith('-'):
+                            row[yield_idx] = '0.00%'
+
+                        alpha_idx = header.index('Alpha')
+                        if row[alpha_idx].strip() == "#DIV/0!":
+                            row[alpha_idx] = '0.00%'
+
+                        ndfdpm_idx = header.index('NDF dpm')
+                        if row[ndfdpm_idx].strip() == "#DIV/0!":
+                            row[ndfdpm_idx] = '0'
+                    
+                        test_time_idx = header.index('Test Time')
+                        row[test_time_idx] = row[test_time_idx].replace('S', '')
+                except ValueError:
+                    print(f"Error: {col} column not found in row {row_idx}")
 
         with open(csv_file, 'w', newline='') as file:
             writer = csv.writer(file)
@@ -73,7 +79,7 @@ def process_folder(folder_path):
 
 def main():
     #folder_path = "C:\\Users\\tanweike\\OneDrive - Intel Corporation\\Desktop\\Dev\\MVT_PHI_Report_PowerBI\\validator"
-    folder_path = "O:\\yield_report\\LatestData\\LatestData\\2024\\07\\data"
+    folder_path = "O:\\yield_report\\LatestData\\LatestData\\2024\\06\\data"
 
     process_folder(folder_path)
 
